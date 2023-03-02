40c49e2af4c176760c3c369c35bb7f33b8de6e5a
gimenes <tlgimenes@gmail.com>
2023-03-02 11:13:22 -0300

    add prefetch strategies

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # Prefetch for Fresh
-Fresh plugin for prefetching pages powered by Google's [quicklink](https://github.com/GoogleChromeLabs/quicklink).
 
-According to [Google's web development guidelines](https://web.dev/link-prefetch/), prefetching leads to faster load times, resulting in higher conversion rates and better user experiences. Prefetching can be implemented [using the link](https://web.dev/codelab-two-ways-to-prefetch/) tag. 
+Fresh plugin for prefetching pages powered by Google's
+[quicklink](https://github.com/GoogleChromeLabs/quicklink).
+
+According to
+[Google's web development guidelines](https://web.dev/link-prefetch/),
+prefetching leads to faster load times, resulting in higher conversion rates and
+better user experiences. Prefetching can be implemented
+[using the link](https://web.dev/codelab-two-ways-to-prefetch/) tag.
 
 ## Usage
 
 To use this plugin, just add to your fresh config:
+
 ```ts
 import prefetchPlugin, { Options } from 'https://deno.land/x/prefetch'
 
@@ -22,6 +29,37 @@ await start(manifest, {
 });
 ```
 
-Start your fresh server, access your page. All anchor tags containing a relative `href` attribute should be prefetched. To make sure it's working, open the Chrome DevTools `Network` tab and make sure the pages are being prefeetched. 
+Start your fresh server, access your page. All anchor tags containing a relative
+`href` attribute should be prefetched. To make sure it's working, open the
+Chrome DevTools `Network` tab and make sure the pages are being prefeetched.
 
 All serializable options accepted by quicklink are also accepted by this plugin.
+
+### Strategies
+
+To allow better control over which links are prefetched, this plugin adds an
+option called `strategy`. When set, two different strategies for prefetching are
+allowed `opt-in`, `opt-out`.
+
+`opt-in` means only links containing the data-prefetch attribute will be
+prefetched. For instance, if you have two links:
+
+```html
+<a href="/foo" data-prefetch />
+<a href="/bar" />
+```
+
+only `/foo` will be prefetched once it enters the viewport
+
+`opt-out` does exactly the opposite from the previous strategy. It prefetches
+all links but those containing `data-noprefetch`. For instance
+
+```html
+<a href="/foo" data-noprefetch />
+<a href="/bar" />
+```
+
+will prefetch `/bar`.
+
+Not setting `options.strategy` will make this plugin fallback to the usual
+quicklink's behavior, i.e. viewport based prefetching
--- mod.ts
+++ mod.ts
@@ -2,33 +2,60 @@ import { Plugin } from "$fresh/server.ts";
 
 export interface Options {
   /** Attempt higher priority fetch (low or high) */
-  priority?: boolean
+  priority?: boolean;
   /** Allowed origins to prefetch (empty allows all) */
-  origins?: string[]
+  origins?: string[];
   /** Timeout after which prefetching will occur */
-  timeout?: number
-  /** 
-   * The concurrency limit for prefetching 
-   * @default 2
-   * */
-  throttle?: number
+  timeout?: number;
+  /**
+   * The concurrency limit for prefetching
+   * @default 10
+   */
+  throttle?: number;
   /** The area percentage of each link that must have entered the viewport to be fetched */
-  threshold?: number
+  threshold?: number;
   /** The total number of prefetches to allow */
-  limit?: number
+  limit?: number;
   /** Time each link needs to stay inside viewport before prefetching (milliseconds) */
-  delay?: number
+  delay?: number;
   /** Option to switch from prefetching and use prerendering only */
-  prerender?: boolean
+  prerender?: boolean;
   /** Option to use both prerendering and prefetching */
-  prerenderAndPrefetch?: boolean
-};
+  prerenderAndPrefetch?: boolean;
+  /**
+   * Strategy to prefetch.
+   *
+   * set opt-in to prefetch only anchor tags containing the data-prefetch attribute
+   * set opt-out to ignore only those links with the data-noprefetch attribute
+   * set aggresive to prefetch all links
+   *
+   * @default undefined
+   */
+  strategy?: "opt-in" | "opt-out";
+}
 
 const prefetch = (options: Options = {
-  throttle: 10
+  throttle: 10,
 }): Plugin => {
   const main = `data:application/javascript,
-    import { listen } from "https://esm.sh/quicklink@2.3.0";
+    import { listen as qlListen } from "https://esm.sh/quicklink@2.3.0";
+
+    function getOptions (options) {
+      if (options.strategy) {
+        options.ignores = [
+          ...(options.ignores ?? []),
+          options.strategy === 'opt-in'
+            ? (_uri, elem) => !elem.hasAttribute('data-prefetch')
+            : (_uri, elem) => elem.hasAttribute('data-noprefetch')
+        ];
+      }
+
+      return options;
+    };
+
+    function listen (options) {
+      return qlListen(getOptions(options))
+    };
 
     export default function(options) {
       if (document.readyState === "complete" || document.readyState === "loaded"  || document.readyState === "interactive") {
@@ -36,20 +63,19 @@ const prefetch = (options: Options = {
       } else {
         document.addEventListener("DOMContentLoaded", () => listen(options));
       }
-    };`
+    };`;
 
   return {
-    name: 'prefetch',
+    name: "prefetch",
     entrypoints: { main },
     render: (ctx) => {
-      ctx.render()
+      ctx.render();
 
       return {
-        scripts: [{ entrypoint: 'main', state: options }]
-      }
-    }
-  }
-}
+        scripts: [{ entrypoint: "main", state: options }],
+      };
+    },
+  };
+};
 
-export default prefetch
-
+export default prefetch;
